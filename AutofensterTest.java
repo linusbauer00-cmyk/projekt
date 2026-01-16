39a7226fc66410096a92708c5aa30d777fae56d5
linusbauer00-cmyk <linusbauer00@gmail.com>
2026-01-16 10:23:31 +0100

    Update AutofensterTest.java

--- AutofensterTest.java
+++ AutofensterTest.java
@@ -14,4 +14,17 @@ class AutofensterTest {
         assertEquals(5,fenster.getAutoliste().size());
     }
 
+    void testdurchschnittspreis(){
+        Autofenster fenster = new Autofenster();
+
+        fenster.getAutoliste().clear();
+        fenster.getAutoliste().add(new Auto("A", "rot", 1000, 10000, false));
+        fenster.getAutoliste().add(new Auto("B", "blau", 1200, 20000, true));
+
+        double ergebnis = fenster.durchschnittsPreis();
+
+        assertEquals(15000.0, ergebnis);
+    }
+
+
 }
